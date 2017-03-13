3cfba163ad17624a28e793936b9854fd7f64d7b2
AnnieSemenova <msnatsu@gmail.com>
2017-03-13 21:16:00 +0300

    profiles

---
+++ Monitoring-web/src/main/java/monitoringweb/beans/generatedControllers/classes/SettingsController.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monitoringweb.beans.generatedControllers.classes;
+
+
+import controllers.rmi.entities.Settings;
+import dao.SettingsFacade;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import monitoringweb.beans.generatedControllers.classes.util.JsfUtil;
+
+/**
+ *
+ * @author annie
+ */
+@Named(value = "settingsController")
+@SessionScoped
+public class SettingsController implements Serializable {
+
+     @EJB
+    private SettingsFacade ejbFacade;
+    private List<Settings> items = null;
+    private Settings selected;
+
+   
+
+    public Settings getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Settings selected) {
+        this.selected = selected;
+    }
+
+    protected void setEmbeddableKeys() {
+    }
+
+    protected void initializeEmbeddableKey() {
+    }
+
+    private SettingsFacade getFacade() {
+        return ejbFacade;
+    }
+
+    public Settings prepareCreate() {
+        selected = new Settings();
+        initializeEmbeddableKey();
+        return selected;
+    }
+
+    public void create() {
+        persist(JsfUtil.PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("SettingsCreated"));
+        if (!JsfUtil.isValidationFailed()) {
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public void update() {
+        persist(JsfUtil.PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("SettingstUpdated"));
+    }
+
+    public void destroy() {
+        persist(JsfUtil.PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("SettingsDeleted"));
+        if (!JsfUtil.isValidationFailed()) {
+            selected = null; // Remove selection
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public List<Settings> getItems() {
+        if (items == null) {
+            items = getFacade().findAll();
+        }
+        return items;
+    }
+
+    private void persist(JsfUtil.PersistAction persistAction, String successMessage) {
+        if (selected != null) {
+            setEmbeddableKeys();
+            try {
+                if (persistAction != JsfUtil.PersistAction.DELETE) {
+                    getFacade().edit(selected);
+                } else {
+                    getFacade().remove(selected);
+                }
+                JsfUtil.addSuccessMessage(successMessage);
+            } catch (EJBException ex) {
+                String msg = "";
+                Throwable cause = ex.getCause();
+                if (cause != null) {
+                    msg = cause.getLocalizedMessage();
+                }
+                if (msg.length() > 0) {
+                    JsfUtil.addErrorMessage(msg);
+                } else {
+                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            }
+        }
+    }
+
+    public Settings getSettings(java.lang.String id) {
+        return getFacade().find(id);
+    }
+
+    public List<Settings> getItemsAvailableSelectMany() {
+        return getFacade().findAll();
+    }
+
+    public List<Settings> getItemsAvailableSelectOne() {
+        return getFacade().findAll();
+    }
+
+    @FacesConverter(forClass = Settings.class)
+    public static class SettingsControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            SettingsController controller = (SettingsController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "settingsController");
+            return controller.getSettings(getKey(value));
+        }
+
+        java.lang.String getKey(String value) {
+            java.lang.String key;
+            key = value;
+            return key;
+        }
+
+        String getStringKey(java.lang.String value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Settings) {
+                Settings o = (Settings) object;
+                return getStringKey(o.getIdSettings());
+            } else {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Settings.class.getName()});
+                return null;
+            }
+        }
+
+    }
+
+    
+}
