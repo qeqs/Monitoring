7defd55afb5998ccadd29d01fa7341eeb623b80b
qeqs <vadimlygin@yandex.ru>
2017-03-20 00:24:08 +0300

    events again...

--- Monitoring-web/src/main/java/monitoringweb/beans/generatedControllers/classes/EventListener.java
+++ Monitoring-web/src/main/java/monitoringweb/beans/generatedControllers/classes/EventListener.java
@@ -2,23 +2,23 @@ package monitoringweb.beans.generatedControllers.classes;
 
 import controllers.rmi.entities.Measure;
 import java.io.Serializable;
-import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 import logic.Listener;
 import logic.events.Event;
 import monitoringweb.beans.generatedControllers.classes.util.JsfUtil;
 import org.primefaces.context.RequestContext;
 
-@ManagedBean
+@Named("eventListener")
+@ApplicationScoped
 public class EventListener implements Serializable {
-    
-    
+
     private Listener listener = new Listener() {
         @Override
         public void onStoreEvent(Event event) {
-            System.out.println(".onStoreEvent() EJJJJHG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             JsfUtil.addMessage(event);
             RequestContext.getCurrentInstance().update("growlEvents");
         }
@@ -27,18 +27,24 @@ public class EventListener implements Serializable {
         public void onStoreMeasure(Measure measure) {
         }
     };
-    public void link(logic.EventController eventController){
+
+    public void link(logic.EventController eventController) {
         eventController.addListener(listener);
     }
-    public void unlink(logic.EventController eventController){
+
+    public void unlink(logic.EventController eventController) {
         eventController.removeListener(listener);
     }
 
+    public void test() {
+        JsfUtil.addErrorMessage("HELLOOOOO");
+        RequestContext.getCurrentInstance().update("growlEvents");
+    }
     @EJB
     private logic.EventController eventController;
-        
+
     @PostConstruct
-    public void postLoad() {        
+    public void postLoad() {
         link(eventController);
     }
 
